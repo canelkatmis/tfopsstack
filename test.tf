11bf337bd58cee8b600034e6e4494dfd42a0bfb0
Can Elkatmış <canelkatmis@gmail.com>
2019-11-20 17:05:18 +0300

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "canelkatmis"
+    workspaces {
+      name = "tfopsstack"
+    }
+  }
+}
+
+resource "null_resource" "root" {
+  triggers = {
+    value = "${timestamp()}"
+  }
+}
